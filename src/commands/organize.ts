950378c2863c97b02749ca50e35497221b68c556
zakaria <himranezakaria2@gmail.com>
2025-09-09 14:02:51 +0200

    Completed the file system logic

--- src/commands/organize.ts
+++ src/commands/organize.ts
@@ -4,6 +4,17 @@ import chalk from 'chalk'
 import { categories } from '../categories.js'
 
 
+const moveFile = (filePath: string, absPath: string, folderName: string, file: string) => {
+  const folderPath = path.join(absPath, folderName)
+
+  if (!fs.existsSync(folderPath)) {
+    fs.mkdirSync(folderPath)
+  }
+
+  fs.renameSync(filePath, path.join(folderPath, file))
+  console.log(chalk.blue(`Moved ${file} to ${folderName}`))
+}
+
 export const organize = (targetPath: string): void => {
   //create an absolute path
   const absPath = path.resolve(targetPath) //An absolute path is the full path starting from the root (C: as example)
@@ -17,12 +28,29 @@ export const organize = (targetPath: string): void => {
   const files: string[] = fs.readdirSync(absPath)
 
   if(files.length === 0){
-    console.log(chalk.yellow('No files founded in this folder'))
+    console.log(chalk.yellow('No files found in this folder'))
   }
 
-  for(const file in files){
+  for(const file of files){
     const filePath = path.join(absPath, file)
-
     const stat: fs.Stats = fs.statSync(filePath) //this synchronously retrieves informations from the file
+
+    if(stat.isFile()){
+      const ext: string = path.extname(filePath).toLowerCase() //extracts the extension name
+      let moved: boolean = false
+
+      for(const [category, extensions] of Object.entries(categories)){
+        if(extensions.includes(ext)){
+          moveFile(filePath, absPath, category, file)
+          moved = true
+          break
+        }
+      }
+      if(!moved){
+        moveFile(filePath, absPath, "others", file)
+      }
+    }
   }
+
+  console.log(chalk.green("Files Organized Successfully"))
 }
